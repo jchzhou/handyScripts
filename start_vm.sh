1cd3949bb1bcd470e2afb13f2009b4e186075583
jchzhou <100064018+jchzhou@users.noreply.github.com>
2022-02-22 21:10:05 +0800

    add start_vm.sh for starting rv64 qemu vm

---
+++ start_vm.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# The script is created for starting a riscv64 qemu virtual machine with specific parameters.
+
+RESTORE=$(echo -en '\001\033[0m\002')
+YELLOW=$(echo -en '\001\033[00;33m\002')
+
+## Configuration
+vcpu=8
+memory=8
+memory_append=`expr $memory \* 1024`
+drive="path/to/your/qcow2"
+fw="path/to/your/fw"
+ssh_port=23333
+### CHANGE ^ IT 
+
+
+cmd="qemu-system-riscv64 \
+  -nographic -machine virt \
+  -smp "$vcpu" -m "$memory"G \
+  -kernel "$fw" \
+  -bios none \
+  -drive file="$drive",format=qcow2,id=hd0 \
+  -object rng-random,filename=/dev/urandom,id=rng0 \
+  -device virtio-rng-device,rng=rng0 \
+  -device virtio-blk-device,drive=hd0 \
+  -device virtio-net-device,netdev=usernet \
+  -netdev user,id=usernet,hostfwd=tcp::"$ssh_port"-:22 \
+  -append 'root=/dev/vda1 rw console=ttyS0 systemd.default_timeout_start_sec=600 selinux=0 highres=off mem="$memory_append"M earlycon' "
+
+echo ${YELLOW}:: Starting VM...${RESTORE}
+echo ${YELLOW}:: Using following configuration${RESTORE}
+echo ""
+echo ${YELLOW}vCPU Cores: "$vcpu"${RESTORE}
+echo ${YELLOW}Memory: "$memory"G${RESTORE}
+echo ${YELLOW}Disk: "$drive"${RESTORE}
+echo ${YELLOW}SSH Port: "$ssh_port"${RESTORE}
+echo ""
+echo ${YELLOW}:: Reminder:${RESTORE}
+echo ${YELLOW}Reboot not feasable at the moment, please use shutdown instead.${RESTORE}
+
+sleep 1
+
+eval $cmd
